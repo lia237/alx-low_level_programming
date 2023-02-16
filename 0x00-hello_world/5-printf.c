0ad4f62530579652f69b1b197bbaf228624615f9
lia237 <ilhamtempo@gmail.com>
2023-02-16 08:39:30 -0800

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 /**
- * main - A simple c program that prints a message to the console using the printf() function
+ * main - A simple c program that prints a message to the console 
+ * using the printf() function
  * Return: 0 indicate successful program execurion
  */
 int main(void)
