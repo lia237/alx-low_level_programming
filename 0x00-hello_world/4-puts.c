c136290226ebc4bba67a893b602e88173b9e12a2
lia237 <ilhamtempo@gmail.com>
2023-02-16 08:13:49 -0800

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * Prints a message to the console using the puts() function.
+ * main - Prints a message to the console using the puts() function.
  */
 int main(void)
 {
-	/* print a message to theconsole */
 	puts("\"Programming is like building a multilingual puzzle");
-	/* Indicate a successful program execution */
+	/* Return: 0 Indicate a successful program execution */
 	return (0);
 }
