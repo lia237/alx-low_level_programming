fa779be7f922c52ccd7fac0f0b3e5bf7bbdcb7a6
lia237 <ilhamtempo@gmail.com>
2023-02-16 09:21:55 -0800

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ int main(void)
 	printf("Size of int: %d byte(s)\n", sizeof(int));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
 	printf("Size of long: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long: %d byte(s)\n", sizeof(long long
-				/ int));
+	printf("Size of long long: %d byte(s)\n", sizeof(long long int));
 	return (0);
 }
