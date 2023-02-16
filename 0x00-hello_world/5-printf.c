1a54242e20103540572c45fd5addaae0dce0c5cf
lia237 <ilhamtempo@gmail.com>
2023-02-16 08:34:46 -0800

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * main - A simple c program that prints a message to the console using the printf() function
  * Return: 0 indicate successful program execurion
  */
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
