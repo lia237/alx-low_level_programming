f9bcd78eb19cae5f88554fc3040d70cb99fd7f67
lia237 <ilhamtempo@gmail.com>
2023-02-17 03:59:25 -0800

    test

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	/*
+	 * main - the main function is the entry point of the program
+	 * Return: the return statement is used to indicate that
+	 * the program has completed successfully
+	 */
+	int n;
+
+	srand(time(NULL));
+	n = rand - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d, is positive\n", n);
+	}
+	if else (n < 0)
+	{
+		printf("%d, is negative\n", n);
+	}
+	else
+		printf("%d, is zero\n", n);
+	return (0);
+}
