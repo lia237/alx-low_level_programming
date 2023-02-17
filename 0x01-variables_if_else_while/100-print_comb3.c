1dee175dcc958d5cf8d1a4bb22a682c8f23ab1c1
lia237 <ilhamtempo@gmail.com>
2023-02-17 09:04:10 -0800

    test

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all possible different combinations of two digits.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int	i1;
+	int	i2;
+
+	i1 = 0;
+	i2 = 0;
+	while (i1 <= 9)
+	{
+		while (i2 <= 9)
+		{
+			putchar((i1 % 10) + '0');
+			putchar((i2 % 10) + '0');
+			if (i1 == 9 && i2 == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+			i2++;
+		}
+		i2 = 0;
+		i1++;
+	}
+	putchar('\n');
+	return (0);
+}
