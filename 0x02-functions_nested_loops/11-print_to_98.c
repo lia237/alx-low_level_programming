5ac982bd6c7eab413a1d1576d5a0d1858223b729
lia237 <ilhamtempo@gmail.com>
2023-02-21 14:48:59 -0800

    test

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,28 +16,15 @@ void print_to_98(int n)
 	{
 		for (i = 0; i <= 98; i++)
 		{
-			if (i != 98)
-			{
-				printf("%d", i);
-			}
-			else if (i == 98)
-			{
-				printf("%d\n", i);
-			}
+			printf("%d ", i);
 		}
 	}
-	else if (n >= 98)
+	else
 	{
 		for (j = 0; j >= 98; j--)
 		{
-			if (j != 98)
-			{
-				printf("%d", j);
-			}
-			else if (j == 98)
-			{
-				printf("%d\n", j);
-			}
+			printf("%d ", i);
 		}
 	}
+	printf("98\n");
 }
