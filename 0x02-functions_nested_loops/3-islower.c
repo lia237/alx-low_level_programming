ca439ab5a32c807ca9b447ed895293dded88b29a
lia237 <ilhamtempo@gmail.com>
2023-02-21 07:05:28 -0800

    test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - checks for lowercase character.
- *
+ * @c: letter being tested
  * Return: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if (c >='a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
