daf9fb3165d60c4665776cd477c1bc910cd2440a
lia237 <ilhamtempo@gmail.com>
2023-02-21 10:06:07 -0800

    test

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,9 @@
 
 /**
  * add - adds two integers and returns the result.
- * @n1:
- * @n2:
- *
- * Return:
+ * @n1: int to be add to n1
+ * @n2: int to be add to n2
+ * Return: the sun of n1 & n2
  */
 
 int add(int n1, int n2)
