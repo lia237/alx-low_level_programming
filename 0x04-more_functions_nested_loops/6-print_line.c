14959b0e92e699b5f0e4f3a5f3e9760caa22bfe4
lia237 <ilhamtempo@gmail.com>
2023-02-23 05:34:23 -0800

    test

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,17 @@ void print_line(int n)
 	int	i;
 
 	if (n <= 0)
-		_putchar('\n');
-	i = 0;
-	while (i < n)
 	{
-		_putchar('_');
-		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
 }
