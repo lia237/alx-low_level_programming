964692d0268bc52d9d36419c28ad7f740e3e571c
lia237 <ilhamtempo@gmail.com>
2023-02-27 07:06:03 -0800

    test

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,19 +11,31 @@ void puts_half(char *str)
 {
 	int	i;
 	int	len;
-	int	start;
+	int	n;
 
+	i = 0;
 	while (str[i])
 	{
 		i++;
 	}
 	len = i;
-	start = (i + 1) / 2;
-	start = i;
-	while (i < len)
+	if (len % 2 == 0)
 	{
-		_putchar(str[i]);
-		i++;
+		i = len / 2;
+		while(str[i])
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else if (len % 2)
+	{
+		n = (len - 1);
+		while (n < len - 1)
+		{
+			_putchar(str[n + 1]);
+			n++;
+		}
 	}
 	_putchar('\n');
 }
