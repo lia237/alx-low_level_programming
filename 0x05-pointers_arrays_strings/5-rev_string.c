fa1871a72efb6b947a72801ce310d3cf43ab4304
lia237 <ilhamtempo@gmail.com>
2023-02-27 04:29:12 -0800

    test

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev -prints a string in reverse
+ * @s: a string
+ *
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	int	i;
+	int	swp;
+	int	j;
+	int	len;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	i--;
+	len = i;
+	j = 0;
+	while (j <  len / 2)
+	{
+		swp = s[j];
+		s[j] = s[len];
+		s[len] = swp;
+		j++;
+		len--;
+	}
+}
