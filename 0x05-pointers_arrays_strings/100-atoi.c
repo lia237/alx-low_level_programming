b308e6920121fa35236cf58465c7e23ea6fc217c
lia237 <ilhamtempo@gmail.com>
2023-02-28 16:05:07 -0800

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: string to convert from
+ *
+ * Return: integer from conversion
+ */
+
+int	_atoi(char *s)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = (result * 10)  + (s[i] - 48);
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				break;
+		}
+		i++;
+	}
+	return (result * sign);
+}
