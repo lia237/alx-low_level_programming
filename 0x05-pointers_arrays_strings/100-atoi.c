133525d3b908bc5f8eb47bbccb0b333df8482133
lia237 <ilhamtempo@gmail.com>
2023-02-28 16:49:27 -0800

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,7 +19,9 @@ int	_atoi(char *s)
 	result = 0;
 	while (s[i])
 	{
-		if (s[i] == '-')
+		if (s[i] == -2147483648)
+			return (-2147483648);
+		else if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
