cc621b8e1ee8d5c8799e341bc3132116dcfc205a
lia237 <ilhamtempo@gmail.com>
2023-03-06 17:02:01 -0800

    jhg

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes..
+ *@s: first value -char
+ *@accept: second value - char
+ *
+ * Return: char with result
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+        int     i;
+        int     j;
+
+        i = 0;
+        j = 0;
+        while (s[i])
+        {
+                while (accept[j])
+                {
+                        if (accept[j] == s[i])
+                                return(&s[i]);
+                        j++;
+                }
+                j = 0;
+                i++;
+        }
+        return (0);
+}
