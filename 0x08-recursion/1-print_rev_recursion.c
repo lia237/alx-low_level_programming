9c7635a82530bc7615996d73253b8fbee40f36ee
lia237 <ilhamtempo@gmail.com>
2023-03-09 03:51:38 -0800

    test

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int	i;
+
+	if (s[i])
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[i];
+	}
+}
