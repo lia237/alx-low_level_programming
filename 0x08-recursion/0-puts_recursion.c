940033064527c0b64c041b84bf8aa60484f34f68
lia237 <ilhamtempo@gmail.com>
2023-03-09 03:28:22 -0800

    test

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,15 @@ void _puts_recursion(char *s)
 {
 	int	i;
 
-	if (s[i])
+	i = 0;
+	if (s[i] == 0)
 	{
-		_putchar(s[i]);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 	else
-		_putchar('\n');
+	{
+		_putchar(s[i]);
+		_putchar(s + 1);
+	}
+
 }
