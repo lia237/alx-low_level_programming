8045ebe922ca75b801e3a13261f19fe7b6b23c75
lia237 <ilhamtempo@gmail.com>
2023-03-14 11:27:43 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * create_array - Entry point
- * @size: size of the array
- * @c: character
- * Return: a pointer to the array, or NULL if it fails
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
  */
 
 char *str_concat(char *s1, char *s2)
@@ -12,17 +13,35 @@ char *str_concat(char *s1, char *s2)
 	int	i;
 	int	len1;
 	int	len2;
+	char	*res;
 
-	w = 0;
-	while(s1[i] && s2[i])
+	i = 0;
+	len1 = 0;
+	len2 = 0;
+	while (s1[len1] && s2[len2])
 	{
-		i++;
+		len1++;
+		len2++;
 	}
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
 	if (s1 == NULL &&  s2 == NULL)
 	{
 		return (NULL);
 	}
 	res = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len1)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (i < len1 + len2)
+	{
+		res[i] = s2[i - len1];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
