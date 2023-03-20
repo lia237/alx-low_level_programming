5760a8e3cb7e0112cb7ce6ac06a6395a35acb117
lia237 <ilhamtempo@gmail.com>
2023-03-20 13:51:58 -0700

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+} dog_t
