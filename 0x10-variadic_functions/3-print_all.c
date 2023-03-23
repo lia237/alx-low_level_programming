f9c15fbf94bb12e2a29d7a5f5e0766567b28a66e
lia237 <ilhamtempo@gmail.com>
2023-03-23 16:50:36 -0700

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,42 +13,42 @@
 
 void print_all(const char * const format, ...)
 {
-	 va_list args;
-	 const char *p;
-	 char c, *s;
-	 int i;
-	 float f;
+	va_list	args;
+	const char	*p;
+	char	c,	*s;
+	int	i;
+	float	f;
 
-	 va_start(args, format);
-	 while (*p)
-	 {
-		 if (*p == 'c')
-		 {
-			 c = (char)va_arg(args, int);
-			 printf("%c", c);
-		 }
-		 else if (*p == 'i')
-		 {
-			 i = va_arg(args, int);
-			 printf("%d", i);
-		 }
-		 else if (*p == 'f')
-		 {
-			 f = (float)va_arg(args, double);
-			 printf("%f", f);
-		 }
-		 else if (*p == 's')
-		 {
-			 s = va_arg(args, char *);
-			 if (s == NULL)
-				 printf("(nil)");
-			 else
-				 printf("%s", s);
-		 }
-		 p++;
-		 if (*p)
-			 printf(", ");
-	 }
-	 va_end(args);
-	 printf("\n");
+	va_start(args, format);
+	while (*p)
+	{
+		if (*p == 'c')
+		{
+			c = (char)va_arg(args, int);
+			printf("%c", c);
+		}
+		else if (*p == 'i')
+		{
+			i = va_arg(args, int);
+			printf("%d", i);
+		}
+		else if (*p == 'f')
+		{
+			f = (float)va_arg(args, double);
+			printf("%f", f);
+		}
+		else if (*p == 's')
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		}
+		p++;
+		if (*p)
+			printf(", ");
+	}
+	va_end(args);
+	printf("\n");
 }
