b30ff592e8baf25b9f5041615940e3166a2f3e36
lia237 <ilhamtempo@gmail.com>
2023-03-22 17:19:45 -0700

    test

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,10 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int	i;
 
 	i = 0;
+	if (!array || !size || !cmp)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
-	return (-1);
 }
