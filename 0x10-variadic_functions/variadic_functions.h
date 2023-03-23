fedf78ca4508189ac1ba67ef035bf3aa59c2aaf8
lia237 <ilhamtempo@gmail.com>
2023-03-23 15:18:16 -0700

    test

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
 
-int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+
 
 #endif
