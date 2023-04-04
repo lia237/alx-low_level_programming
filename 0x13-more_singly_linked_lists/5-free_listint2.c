977d1d59fbe65ac31a7541ce1a5c4b52c672c287
lia237 <ilhamtempo@gmail.com>
2023-04-04 14:01:03 -0700

    test

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: head node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t	*tmp;
+
+	if (head == NULL)
+		return;
+	while (head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
