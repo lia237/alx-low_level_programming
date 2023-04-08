9bf58ba24ab4aacdfc6fde75210ed4d969e92bef
lia237 <ilhamtempo@gmail.com>
2023-04-08 07:49:49 -0700

    test

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_binary(unsigned long int n)
+{
+	int	i;
+	unsigned long int	mask;
+
+	mask =  1;
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 63; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			break;
+		}
+		mask <<= 1;
+	}
+	while (mask)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
+}
