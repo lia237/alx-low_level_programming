fe46cbc55ecd9abbda320c1936be0a7abb9d34c1
lia237 <ilhamtempo@gmail.com>
2023-04-08 09:11:07 -0700

    test

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,9 +2,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int	mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return -1;
-	unsigned long int mask = (unsigned long int) 1 << index;
+	mask = (unsigned long int) 1 << index;
 	*n |= mask;
 	return 1;
 }
