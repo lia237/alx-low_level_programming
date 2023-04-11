cf9122e3ddf2550b52b91a7a406561a045bb6d2d
lia237 <ilhamtempo@gmail.com>
2023-04-10 18:50:38 -0700

    test

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * append_text_to_file - Appends text at the end of a file.
@@ -19,6 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
+	i = 0;
 	while (text_content[i] != '\0')
 	{
 		i++;
