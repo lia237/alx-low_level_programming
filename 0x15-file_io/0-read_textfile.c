b6604c780f02c2d472fd1abf81cc1ad040e1bfe5
lia237 <ilhamtempo@gmail.com>
2023-04-10 18:21:41 -0700

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	r = read(f, buffer, letters);
-	w = write(1, buffer, r);
+	w = write(STDIN_FILENO, buffer, r);
 	close(f);
 	return (w);
 }
